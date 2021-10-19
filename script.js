5fc7f45671396f00ff2a6f3148d862ca7d8fe4f9
Mr. Mueller <bmueller@hstat.org>
2021-10-19 19:12:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+/* INSTRUCTIONS */
+// "refactor" means to to improve existing code without changing the result
+// So before changing the code, make sure you press "run" to see what the result/output is. You should see the favorite number change from 5 to 10.
+// Refactor the program so that it uses a function (overall less lines of code) but STILL shows the SAME output!
 
+var favNum = 5;
+document.write("My favorite number is " + favNum + ".<br>");
+favNum++;
+document.write("No, wait! My favorite number is " + favNum + ".<br>");
+favNum++;
+document.write("No, wait! My favorite number is " + favNum + ".<br>");
+favNum++;
+document.write("No, wait! My favorite number is " + favNum + ".<br>");
+favNum++;
+document.write("No, wait! My favorite number is " + favNum + ".<br>");
+favNum++;
+document.write("Ok, I made up my mind! My REAL favorite number is... " + favNum + "!");
